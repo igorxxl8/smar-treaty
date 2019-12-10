3e114edcdeffc081eb8183486fce8e7ea343e3f3
Anton Rekuts <walot04@gmail.com>
2019-12-10 04:57:39 +0300

    Deploing contract from web added

---
+++ src/SmarTreaty.Common.ViewModels/Templates/CreateTemplateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SmarTreaty.Common.DomainModel;
+
+namespace SmarTreaty.Common.ViewModels.Templates
+{
+    public class CreateTemplateViewModel
+    {
+        public string Source { get; set; }
+        public string Name { get; set; }
+        public bool Verified { get; set; }
+        public string Abi { get; set; }
+        public string ByteCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
--- src/SmarTreaty/Controllers/ContractsController.cs
+++ src/SmarTreaty/Controllers/ContractsController.cs
@@ -34,24 +34,6 @@ namespace SmarTreaty.Controllers
         // GET: Contracts
         public ActionResult Index()
         {
-
-            //var res = await _smartContractService.CompileContract(@"pragma solidity >=0.4.22 <0.6.0;contract test_xtemplate {int _multiplier;constructor(int init) public {}function test_(int multiplier) public {_multiplier = multiplier;}function multiply(int val) public returns(int result) {result = val * _multiplier;}}");
-            //var k = JsonConvert.DeserializeObject<object[]>(res);
-            //var template = new SmartContract
-            //{
-            //    Abi = JsonConvert.SerializeObject(k[0]),
-            //    ByteCode = (string)k[1]
-            //};
-            //var a = new object[] { 1 };
-
-            //try
-            //{
-            //    await _smartContractService.DeployContract(template, "0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7");
-            //}
-            //catch
-            //{
-
-            //}
             return View();
         }
 
